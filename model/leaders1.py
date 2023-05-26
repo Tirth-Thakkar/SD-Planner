9eb6a650890f188d73d04c3abe9da625d0c2f612
Haoxuan Tong <haoxuantong@gmail.com>
2023-05-26 12:22:50 -0700

    new file:   model/leaders1.py

---
+++ model/leaders1.py
@@ -0,0 +1,106 @@
+import json
+from __init__ import app, db
+from sqlalchemy.exc import IntegrityError
+
+class Leader(db.Model):
+    __tablename__ = 'Leaderboard'  # table name is plural, class name is singular
+
+    # Define the User schema with "vars" from object
+    id = db.Column(db.Integer, primary_key=True)
+    _name = db.Column(db.String(255), unique=False, nullable=False)
+    _score = db.Column(db.Integer, unique=False, nullable=False)
+    _score = db.Column(db.Integer, unique=False, nullable=False)
+    _score = db.Column(db.Integer, unique=False, nullable=False)
+    _score = db.Column(db.Integer, unique=False, nullable=False)
+
+
+    # constructor of a User object, initializes the instance variables within object (self)
+    def __init__(self, name, score):
+        self._name = name    # variables with self prefix become part of the object, 
+        self._score = score
+
+
+    # a name getter method, extracts name from object
+    @property
+    def name(self):
+        return self._name
+    
+    # a setter function, allows name to be updated after initial object creation
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
+    # a getter method, extracts email from object
+    @property
+    def score(self):
+        return self._score
+    
+    # a setter function, allows name to be updated after initial object creation
+    @score.setter
+    def score(self, score):
+        self._score = score
+           
+   
+    # output content using str(object) in human readable form, uses getter
+    # output content using json dumps, this is ready for API response
+    def __str__(self):
+        return json.dumps(self.read())
+
+    # CRUD create/add a new record to the table
+    # returns self or None on error
+    def create(self):
+        try:
+            # creates a person object from User(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
+            return self
+        except IntegrityError:
+            db.session.remove()
+            return None
+
+    # CRUD read converts self to dictionary
+    # returns dictionary
+    def read(self):
+        return {
+            "name": self.name,
+            "score": self.score,
+        }
+
+    # CRUD update: updates user name, password, phone
+    # returns self
+    def update(self, name, score):
+        """only updates values with length"""
+        if len(name) > 0:
+            self.name = name
+        if score > 0:
+            self.score = score
+        db.session.commit()
+        return self
+
+    # CRUD delete: remove self
+    # None
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        return None
+
+
+def initLeaders():
+    with app.app_context():
+        """Create database and tables"""
+        db.create_all()
+        """Tester data for table"""
+        score1 = Leader(name='Chester', score = 100)
+        score2 = Leader(name='Sonic Hedgehog', score = 15)
+        score3 = Leader(name='Megamind', score = 3)
+        score4 = Leader(name='Gamer Man', score = 215)
+
+        leaders = [score1, score2, score3, score4]
+
+        for leader in leaders:
+            try:
+                leader.create()
+            except IntegrityError:
+                db.session.remove()
+                print(f"error try again later")
+            
