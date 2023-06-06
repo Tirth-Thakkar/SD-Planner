08ba11462602052aac3ab93e6c18ad4b18f87c46
JishnuS420 <broburn123@gmail.com>
2023-06-06 10:36:54 -0700

    login works

--- api/login.py
+++ api/login.py
@@ -1,71 +1,76 @@
+import json
 from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource # used for REST API building
+from datetime import datetime
 
 from model.logins import Login
 
-# Change variable name and API name and prefix
 login_api = Blueprint('login_api', __name__,
-                   url_prefix='/api/login')
+                   url_prefix='/api/logins')
 
 # API docs https://flask-restful.readthedocs.io/en/latest/api.html
 api = Api(login_api)
 
-class LoginAPI:     
-    class AddLogin(Resource):
-        def post(self):
+class LoginAPI:        
+    class Cred(Resource):  # User API operation for Create and Read
+        def post(self): # Create method
             ''' Read data for json body '''
             body = request.get_json()
             
             ''' Avoid garbage in, error checking '''
-            # validate name
-            email = body.get('email')
-            if email is None or len(email) < 5:
-                return {'message': f'Email is missing, or is less than 2 characters'}, 210
+            # validate uid
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
+            # look for password and dob
             password = body.get('password')
 
-            ''' #1: Key code block, setup PLAYER OBJECT '''
-            po = Login(email = email)
+            ''' #1: Key code block, setup USER OBJECT '''
+            uo = Login(uid=uid)
             
             ''' Additional garbage error checking '''
             # set password if provided
             if password is not None:
-                po.set_password(password)            
+                uo.set_password(password)
+            # convert to date type
             
             ''' #2: Key Code block to add user to database '''
-            # create player in database
-            login = po.create()
-            # success returns json of player
+            # create user in database
+            login = uo.create()
+            # success returns json of user
             if login:
                 return jsonify(login.read())
             # failure returns error
-            return {'message': f'Processed {email}, either a format error or email is duplicate'}, 210
-        
-    class GetLogins(Resource):
-        def get(self):
-            logins = Login.query.all()    # read/extract all players from database
+            return {'message': f'Processed {uid}, either a format error or User ID {uid} is duplicate'}, 400
+
+        def get(self): # Read Method
+            logins = Login.query.all()    # read/extract all users from database
             json_ready = [login.read() for login in logins]  # prepare output in json
             return jsonify(json_ready)  # jsonify creates Flask response object, more specific to APIs than json.dumps
-        
-    class UpdateLogin(Resource):
-        def put(self):
-            body = request.get_json() # get the body of the request
-            data = body.get('data')
-            email = body.get('email')
-            login = Login.query.get(email) # get the player (using the uid in this case)
-            login.update(data)
-            return f"{login.read()} Updated"
+    
+    class Search(Resource):
 
-    class DeleteLogin(Resource):
-        def delete(self):
+        def post(self):
+            ''' Read data for json body '''
             body = request.get_json()
-            email = body.get('email')
-            login = Login.query.get(email)
-            login.delete()
-            return f"{login.read()} Has been deleted"
+            
+            ''' Get Data '''
+            uid = body.get('uid')
+            if uid is None or len(uid) < 2:
+                return {'message': f'User ID is missing, or is less than 2 characters'}, 400
+            password = body.get('password')
+            
+            ''' Find user '''
+            login = Login.query.filter_by(_uid=uid).first()
+            if login is None or not login.is_password(password):
+                return {'message': f"Invalid user id or password"}, 400
+            
+            ''' authenticated user '''
+            return jsonify(login.read())
 
+            
 
-    # building RESTapi endpoint, method distinguishes action
-    api.add_resource(AddLogin, '/logins')
-    api.add_resource(GetLogins, '/ulogin')
-    api.add_resource(UpdateLogin, '/uplogin')
-    api.add_resource(DeleteLogin, '/dlogin')   
+    # building RESTapi endpoint
+    api.add_resource(Cred, '/logins')
+    api.add_resource(Search, '/lsearch')
+    
--- model/logins.py
+++ model/logins.py
@@ -1,110 +1,135 @@
+""" database dependencies to support sqliteDB examples """
 from random import randrange
 from datetime import date
-import os
-import base64
+import os, base64
 import json
 
 from __init__ import app, db
 from sqlalchemy.exc import IntegrityError
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# Define the Login class to manage user login actions in the 'logins' table
+
+''' Tutorial: https://www.sqlalchemy.org/library.html#tutorials, try to get into Python shell and follow along '''
+
+# Define the Post class to manage actions in 'posts' table,  with a relationship to 'users' table
+
+# Define the User class to manage actions in the 'users' table
+# -- Object Relational Mapping (ORM) is the key concept of SQLAlchemy
+# -- a.) db.Model is like an inner layer of the onion in ORM
+# -- b.) User represents data we want to store, something that is built on db.Model
+# -- c.) SQLAlchemy ORM is layer on top of SQLAlchemy Core, then SQLAlchemy engine, SQL
 class Login(db.Model):
     __tablename__ = 'logins'  # table name is plural, class name is singular
 
-    # Define the Login schema with variables from the object
+    # Define the Login schema with "vars" from object
     id = db.Column(db.Integer, primary_key=True)
-    _email = db.Column(db.String(255), unique=False, nullable=False)
+    _uid = db.Column(db.String(255), unique=True, nullable=False)
     _password = db.Column(db.String(255), unique=False, nullable=False)
 
-    # Constructor of a Login object, initializes the instance variables within the object (self)
-    def __init__(self, email, password):
-        self._email = email
-        self._password = password
 
-    # Getter method to retrieve email from the object
+    # constructor of a User object, initializes the instance variables within object (self)
+    def __init__(self, uid, password="123qwerty"):
+        # variables with self prefix become part of the object, 
+        self._uid = uid
+        self.set_password(password)
+    
+    # a getter method, extracts username from object
     @property
-    def email(self):
-        return self._email
-
-    # Setter function to update the email after initial object creation
-    @email.setter
-    def email(self, email):
-        self._email = email
-
-    # Getter method to retrieve password from the object
+    def uid(self):
+        return self._uid
+    
+    # a setter function, allows name to be updated after initial object creation
+    @uid.setter
+    def uid(self, uid):
+        self._uid = uid
+        
+    # check if uid parameter matches user id in object, return boolean
+    def is_uid(self, uid):
+        return self._uid == uid
+    
     @property
     def password(self):
-        return self._password
+        return self._password[0:10] + "..." # because of security only show 1st characters
 
-    # Setter function to update the password after initial object creation
-    @password.setter
-    def password(self, password):
-        self._password = password
+    # update password, this is conventional setter
+    def set_password(self, password):
+        """Create a hashed password."""
+        self._password = generate_password_hash(password, method='sha256')
 
-    # Output content using str(object) in human-readable form
+    # check password parameter versus stored/encrypted password
+    def is_password(self, password):
+        """Check against hashed password."""
+        result = check_password_hash(self._password, password)
+        return result
+
+    # output content using str(object) in human readable form, uses getter
+    # output content using json dumps, this is ready for API response
     def __str__(self):
         return json.dumps(self.read())
 
     # CRUD create/add a new record to the table
+    # returns self or None on error
     def create(self):
         try:
-            db.session.add(self)
-            db.session.commit()
+            # creates a person object from User(db.Model) class, passes initializers
+            db.session.add(self)  # add prepares to persist person object to Users table
+            db.session.commit()  # SqlAlchemy "unit of work pattern" requires a manual commit
             return self
         except IntegrityError:
             db.session.remove()
             return None
 
-    # CRUD read converts self to a dictionary
+    # CRUD read converts self to dictionary
+    # returns dictionary
     def read(self):
         return {
             "id": self.id,
-            "email": self.email,
-            "password": self.password,
+            "uid": self.uid,
         }
 
-    # CRUD update: updates user email and password
-    def update(self, email="", password=""):
-        """Only updates values with length"""
-        if len(email) > 0:
-            self.email = email
+    # CRUD update: updates user name, password, phone
+    # returns self
+    def update(self, uid="", password=""):
+        """only updates values with length"""
+        if len(uid) > 0:
+            self.uid = uid
         if len(password) > 0:
-            self.password = password
+            self.set_password(password)
         db.session.commit()
         return self
 
     # CRUD delete: remove self
+    # None
     def delete(self):
         db.session.delete(self)
         db.session.commit()
         return None
 
 
-"""Database Creation and Testing"""
+"""Database Creation and Testing """
 
 
 # Builds working data for testing
 def initLogins():
     with app.app_context():
         """Create database and tables"""
-        db.init_app(app)
         db.create_all()
-
         """Tester data for table"""
-        u1 = Login(email="jishnu@gmail.com", password='123jishnu')
-        u2 = Login(email="alan@gmail.com", password='123alan')
-        u3 = Login(email="tirth@gmail.com", password='123tirth')
-        u4 = Login(email="yuri@gmail.com", password='123yuri')
-        u5 = Login(email="haoxuan@gmail.com", password='123haoxuan')
+        u1 = Login(uid='JishnuS420', password='123jishnu')
+        u2 = Login(uid='TirthThakkar', password='123tirth')
+        u3 = Login(uid='A1234l',  password='123alan')
+        u4 = Login(uid='yuricoder07', password='123yuri')
+        u5 = Login(uid='JasoXDDD', password='123haoxuan')
 
         logins = [u1, u2, u3, u4, u5]
 
         """Builds sample user/note(s) data"""
         for login in logins:
             try:
+                '''add login/post data to table'''
                 login.create()
             except IntegrityError:
-                '''Fails with bad or duplicate data'''
+                '''fails with bad or duplicate data'''
                 db.session.remove()
-                print(f"Records exist, duplicate email, or error: {login.email}")
+                print(f"Records exist, duplicate email, or error: {login.uid}")
+            
